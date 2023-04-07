415bfb4dd2f3c88ca5b6845e474ff37c9ce9165d
Mathieu Imfeld <imfeldma@gmail.com>
2023-04-07 10:41:57 +0200

    Inception

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# mrmat-conditional-release-action
-A GitHub action to perform a conditional release as I like it
+# MrMat :: Conditional Release Action
+
+A GitHub action to perform a conditional project release the way I like it
+
+## Inputs
+
+### env_version
+
+The environment variable holding the version, used for the tag and release object
+
+### title
+
+**Required** The release title
+
+## Outputs
+
+None
+
+## Example usage
+
+```yaml
+uses: actions/mrmat-conditional-release-action@v1
+with:
+  title: "Release ${{ env.MRMAT_VERSION }}"
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    //
+    // Initialise
+    core.startGroup('MrMat :: Conditional Release')
+    const version = core.getInput('version')
+    const title = core.getInput('title')
+    const VERSION = process.env.MRMAT_VERSION
+    const OWNER = process.env.GITHUB_REPOSITORY_OWNER
+    const REPOSITORY = process.env.GITHUB_REPOSITORY.split('/')[1]
+    const COMMIT = process.env.GITHUB_SHA
+    const now = (new Date()).toISOString()
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+
+    //
+    // Get identity information about the tagger
+
+    const { data: user } = await octokit.rest.user()
+    core.info('User fetched: ' + user)
+
+    //
+    // Create a tag
+
+    // await octokit.rest.git.createTag({
+    //     'owner': OWNER,
+    //     'repo': REPOSITORY,
+    //     'tag': VERSION,
+    //     'message': title,
+    //     'object': COMMIT,
+    //     'type': 'commit',
+    //     'tagger': {
+    //         'name': user.name,
+    //         'email': user.email,
+    //         'date': now
+    //     }
+    // })
+
+} catch(error) {
+    core.setFailed(error.message)
+}
